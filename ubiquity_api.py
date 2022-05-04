8f7f7f801151787982af33baade9152eaff36357
Federico <nellenfederico@gmail.com>
2022-05-04 22:19:42 +0200

    Ubiquiti -> Unifi, port for Gateway added

--- ubiquity_api.py
+++ ubiquity_api.py
@@ -9,7 +9,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # https://github.com/Art-of-WiFi/UniFi-API-client/blob/cbe89d913cc4dd3742d5fd35e201ec86bc8ccdc9/src/Client.php#L1919
 
 
-class Voucher:
+class UniFiVoucher:
 
     def __init__(self, id, adminName, code, creationTime, duration, note, speedUp, speedDown, usageQuota, siteId, status, used, statusExipres) -> None:
         self.id = id
@@ -42,14 +42,15 @@ class Voucher:
         self.statusExipres = json["status_expires"]
 
 
-class UbiquitiClient:
+class UniFiClient:
     # set up connection parameters in a dictionary
-    gateway = {"ip": ""}
+    gateway = {"ip": "", "port": ""}
     csrf_token = ""
     session = None
 
-    def __init__(self, gateway_ip) -> None:
-        self.gateway["ip"] = str(gateway_ip)
+    def __init__(self, gatewayIp, gatewayPort) -> None:
+        self.gateway["ip"] = str(gatewayIp)
+        self.gateway["gatewayPort"] = str(gatewayPort)
         self.csrf_token, self.session = self.getCsrfToken()
 
     def getCsrfToken(self):
@@ -78,21 +79,21 @@ class UbiquitiClient:
      
      NOTES: please use the retrieveVoucher() method/function to retrieve the newly created voucher(s) by create_time
      
-     @param int    $minutes   minutes the voucher is valid after activation (expiration time)
-     @param int    $count     number of vouchers to create, default value is 1
-     @param int    $quota     single-use or multi-use vouchers, value '0' is for multi-use, '1' is for single-use,
+     @param int    minutes   minutes the voucher is valid after activation (expiration time)
+     @param int    count     number of vouchers to create, default value is 1
+     @param int    quota     single-use or multi-use vouchers, value '0' is for multi-use, '1' is for single-use,
                               'n' is for multi-use n times
-     @param string $note      note text to add to voucher when printing
-     @param int    $up        upload speed limit in kbps
-     @param int    $down      download speed limit in kbps
-     @param int    $megabytes data transfer limit in MB
+     @param string note      note text to add to voucher when printing
+     @param int    up        upload speed limit in kbps
+     @param int    down      download speed limit in kbps
+     @param int    megabytes data transfer limit in MB
      @return array containing a single object which contains voucher createtime
      */
      """
 
     def createVoucher(self, minutes, count, quota, note, up=None, down=None, megabytes=None):
         createVoucherUrl = f"proxy/network/api/s/default/cmd/hotspot"
-        url = f"https://{self.gateway['ip']}/{createVoucherUrl}"
+        url = f"https://{self.gateway['ip']}:{self.gateway['port']}/{createVoucherUrl}"
         headers = {
             "Content-Type": "application/json",
             "x-csrf-token": self.csrf_token
@@ -114,7 +115,7 @@ class UbiquitiClient:
 
     def retrieveVoucher(self, create_time):
         fetchVoucherUrl = f"proxy/network/api/s/default/stat/voucher"
-        url = f"https://{self.gateway['ip']}/{fetchVoucherUrl}"
+        url = f"https://{self.gateway['ip']}:{self.gateway['port']}/{fetchVoucherUrl}"
         headers = {
             "Content-Type": "application/json",
             "x-csrf-token": self.csrf_token
@@ -127,12 +128,12 @@ class UbiquitiClient:
         api_data = response.json()
         vouchers = []
         for voucher in api_data["data"]:
-            vouchers.append(Voucher(voucher))
+            vouchers.append(UniFiVoucher(voucher))
         return vouchers
 
     def revokeVoucher(self, voucherId):
         deleteVoucherUrl = f"proxy/network/api/s/default/cmd/hotspot"
-        url = f"https://{self.gateway['ip']}/{deleteVoucherUrl}"
+        url = f"https://{self.gateway['ip']}:{self.gateway['port']}/{deleteVoucherUrl}"
         headers = {
             "Content-Type": "application/json",
             "x-csrf-token": self.csrf_token
@@ -144,18 +145,18 @@ class UbiquitiClient:
         response = self.session.post(url, headers=headers,
                                      data=json.dumps(body), verify=False)
         api_data = response.json()
-        pprint(api_data)
+        return api_data
 
 
 if __name__ == "__main__":
     # create a new client
-    client = UbiquitiClient("192.168.1.1")
+    client = UniFiClient("192.168.1.1", "443")
     # create a voucher
     voucherCreated = client.createVoucher(
         minutes=4320, count=5, quota=1, note="test 3 days", up=100, down=100, megabytes=100)
-    pprint(voucherCreated)
     # retrieve the voucher
     vouchers = client.retrieveVoucher(voucherCreated[0]["create_time"])
     for voucher in vouchers:
         print(voucher.id)
         print(voucher.code)
+        client.revokeVoucher(voucherId=voucher.id)
