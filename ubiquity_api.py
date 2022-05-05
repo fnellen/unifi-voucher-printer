53fe9e8e372916e669a3be100ddcec1d14981f4a
Federico <nellenfederico@gmail.com>
2022-05-05 11:45:19 +0200

    Error Handling

--- ubiquity_api.py
+++ ubiquity_api.py
@@ -1,4 +1,3 @@
-from http import cookies
 import requests
 import json
 import urllib3
@@ -11,7 +10,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class UniFiVoucher:
 
-    def __init__(self, id, adminName, code, creationTime, duration, note, speedUp, speedDown, usageQuota, siteId, status, used, statusExipres) -> None:
+    def __init__(self, id, adminName, code, creationTime, duration, note, speedUp, speedDown, usageQuota, siteId, status, used, statusExpires) -> None:
         self.id = id
         self.adminName = adminName
         self.code = code
@@ -26,20 +25,23 @@ class UniFiVoucher:
         self.used = used
         self.statusExpires = statusExpires
 
-    def __init__(self, json):
-        self.id = json["_id"]
-        self.adminName = json["admin_name"]
-        self.code = json["code"]
-        self.creationTime = json["create_time"]
-        self.duration = json["duration"]
-        self.note = json["note"]
-        self.speedUp = json["qos_rate_max_up"]
-        self.speedDown = json["qos_rate_max_down"]
-        self.usageQuota = json["qos_usage_quota"]
-        self.siteId = json["site_id"]
-        self.status = json["status"]
-        self.used = json["used"]
-        self.statusExipres = json["status_expires"]
+    def __init__(self, json: dict):
+        self.id = json.get("_id", None)
+        self.adminName = json.get("admin_name", None)
+        self.code = json.get("code", None)
+        self.creationTime = json.get("create_time", None)
+        self.duration = json.get("duration", None)
+        self.note = json.get("note", None)
+        self.speedUp = json.get("qos_rate_max_up", None)
+        self.speedDown = json.get("qos_rate_max_down", None)
+        self.usageQuota = json.get("qos_usage_quota", None)
+        self.siteId = json.get("site_id", None)
+        self.status = json.get("status", None)
+        self.used = json.get("used", None)
+        self.statusExpires = json.get("status_expires", None)
+
+    def __str__(self) -> str:
+        return f"ID: {self.id}, Created By: {self.adminName}, Code: {self.code}, Created At {self.creationTime}, Valid for: {self.duration}, Note: {self.note}, Upload Speed: {self.speedUp}, Download Speed {self.speedDown}, Quota: {self.usageQuota}, SiteId: {self.siteId}, Status: {self.status}, Used: {self.used}, Status Expires: {self.statusExpires}"
 
 
 class UniFiClient:
@@ -66,13 +68,16 @@ class UniFiClient:
             "password": config("PASSWORD")
         }
         # Open a session for capturing cookies
-        session = requests.Session()
-        # login
-        response = session.post(url, headers=headers,
-                                data=json.dumps(body), verify=False)
-
-        # Get CSRF token from response headers
-        return response.headers["X-CSRF-Token"], session
+        try:
+            session = requests.Session()
+            # login
+            response = session.post(url, headers=headers,
+                                    data=json.dumps(body), verify=False)
+            response.raise_for_status()
+            # Get CSRF token from response headers
+            return response.headers["X-CSRF-Token"], session
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
 
     """
      Create voucher(s)
@@ -108,10 +113,17 @@ class UniFiClient:
             "down": down,
             "bytes": megabytes,
         }
-        response = self.session.post(url, headers=headers,
-                                     data=json.dumps(body), verify=False)
-        api_data = response.json()
-        return api_data["data"]
+        try:
+            response = self.session.post(url, headers=headers,
+                                         data=json.dumps(body), verify=False)
+            response.raise_for_status()
+            api_data = response.json()
+            vouchers = []
+            for voucher in api_data["data"]:
+                vouchers.append(UniFiVoucher(voucher))
+            return vouchers
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
 
     def retrieveVoucher(self, create_time):
         fetchVoucherUrl = f"proxy/network/api/s/default/stat/voucher"
@@ -125,11 +137,36 @@ class UniFiClient:
         }
         response = self.session.post(url, headers=headers,
                                      data=json.dumps(body), verify=False)
-        api_data = response.json()
-        vouchers = []
-        for voucher in api_data["data"]:
-            vouchers.append(UniFiVoucher(voucher))
-        return vouchers
+        response.raise_for_status()
+        try:
+            api_data = response.json()
+            vouchers = []
+            for voucher in api_data["data"]:
+                vouchers.append(UniFiVoucher(voucher))
+            return vouchers
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
+
+    def retrieveAllVouchers(self) -> list:
+        fetchVoucherUrl = f"proxy/network/api/s/default/stat/voucher"
+        url = f"https://{self.gateway['ip']}:{self.gateway['port']}/{fetchVoucherUrl}"
+        headers = {
+            "Content-Type": "application/json",
+            "x-csrf-token": self.csrf_token
+        }
+        body = {
+        }
+        response = self.session.post(url, headers=headers,
+                                     data=json.dumps(body), verify=False)
+        response.raise_for_status()
+        try:
+            api_data = response.json()
+            vouchers = []
+            for voucher in api_data["data"]:
+                vouchers.append(UniFiVoucher(voucher))
+            return vouchers
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
 
     def revokeVoucher(self, voucherId):
         deleteVoucherUrl = f"proxy/network/api/s/default/cmd/hotspot"
@@ -144,8 +181,11 @@ class UniFiClient:
         }
         response = self.session.post(url, headers=headers,
                                      data=json.dumps(body), verify=False)
-        api_data = response.json()
-        return api_data
+        response.raise_for_status()
+        try:
+            return True
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
 
 
 if __name__ == "__main__":
@@ -154,9 +194,14 @@ if __name__ == "__main__":
     # create a voucher
     voucherCreated = client.createVoucher(
         minutes=4320, count=5, quota=1, note="test 3 days", up=100, down=100, megabytes=100)
+    voucherCreated = client.createVoucher(
+        minutes=2880, count=10, quota=1, note="test 5 days", up=100, down=100, megabytes=100)
     # retrieve the voucher
-    vouchers = client.retrieveVoucher(voucherCreated[0]["create_time"])
+    vouchers = client.retrieveAllVouchers()
+    for v in vouchers:
+        print(v)
+    vouchers = client.retrieveVoucher(voucherCreated[0].creationTime)
     for voucher in vouchers:
         print(voucher.id)
         print(voucher.code)
-        client.revokeVoucher(voucherId=voucher.id)
+        #print("Deleted: " + str(client.revokeVoucher(voucherId=voucher.id)))
