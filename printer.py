8a543faef5385c7980d3525e1325b1ad9db62a4f
Federico <nellenfederico@gmail.com>
2022-05-05 23:00:27 +0200

    Error handling for printing

--- printer.py
+++ printer.py
@@ -1,6 +1,5 @@
-from sre_constants import SUCCESS
 from uniFiVouchers import UniFiVoucher
-import os
+import subprocess
 # https://github.com/pklaus/brother_ql
 
 
@@ -16,11 +15,15 @@ class PrinterSpooler:
         for voucher in vouchers:
             # Save pages as images in the pdf
             # brother_ql -b pyusb -m 'QL-570' -p usb://0x04f9:0x2028 print -l '62x29' {voucher.id}
-            stream = os.popen(
-                f"brother_ql -b {self.bus} -m '{self.deviceModel}' -p {self.devicePort} print -l '{self.imgSize}' tmp/{voucher.id}.png")
-            output = stream.read()
-            # if "Printing was successful" in output:
-            #     print(SUCCESS)
-            # else:
-            #     print(f"Error while printing voucher {voucher.id}")
-            #     break
+            # successfull = os.system(
+            #    f"brother_ql -b {self.bus} -m '{self.deviceModel}' -p {self.devicePort} print -l '{self.imgSize}' tmp/{voucher.id}.png")
+            process = subprocess.Popen(['brother_ql', '-b', self.bus, '-m', self.deviceModel, '-p', self.devicePort, 'print', '-l', self.imgSize, f'tmp/{voucher.id}.png'],
+                                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+            if "Printing was successful" in stdout.decode('utf-8'):
+                print(f"Printing was successful for {voucher.id}")
+            elif "Printing was successful" in stderr.decode('utf-8'):
+                print(f"Printing was successful for {voucher.id}")
+            else:
+                # print(stderr)
+                print(f"Printing failed for {voucher.id}")
