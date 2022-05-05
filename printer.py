11d20d3d90cb954cf71a1763dfdf4d9db3b12ba8
Federico <nellenfederico@gmail.com>
2022-05-05 23:02:44 +0200

    removed comment

--- printer.py
+++ printer.py
@@ -13,10 +13,6 @@ class PrinterSpooler:
 
     def printImgs(self, vouchers: list[UniFiVoucher]):
         for voucher in vouchers:
-            # Save pages as images in the pdf
-            # brother_ql -b pyusb -m 'QL-570' -p usb://0x04f9:0x2028 print -l '62x29' {voucher.id}
-            # successfull = os.system(
-            #    f"brother_ql -b {self.bus} -m '{self.deviceModel}' -p {self.devicePort} print -l '{self.imgSize}' tmp/{voucher.id}.png")
             process = subprocess.Popen(['brother_ql', '-b', self.bus, '-m', self.deviceModel, '-p', self.devicePort, 'print', '-l', self.imgSize, f'tmp/{voucher.id}.png'],
                                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = process.communicate()
