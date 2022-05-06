907d66031be6ceb1a2c14cf731293dbf3735e8b4
Federico <nellenfederico@gmail.com>
2022-05-06 19:23:57 +0200

    eslint config

--- www/unifi-voucher-service/next.config.js
+++ www/unifi-voucher-service/next.config.js
@@ -1,6 +1,33 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+          },
+        },
+      ],
+    });
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: "/docs",
+        destination: "/docs/welcome",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
