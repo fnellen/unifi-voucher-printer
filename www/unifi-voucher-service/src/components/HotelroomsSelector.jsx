831f0ab420e5a14ec5b27f9ebc1d1b2da8002b36
Federico <nellenfederico@gmail.com>
2022-05-06 23:29:50 +0200

    Components

---
+++ www/unifi-voucher-service/src/components/HotelroomsSelector.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+import styles from '../../styles/Home.module.css';
+
+export default function HotelroomsSelector(props) {
+    
+        const [selectedRoom, setSelectedRoom] = useState(0)
+
+        const hotelrooms = [[100, 101, 102, 103, 104, 105, 106], [200, 201, 202, 203, 204, 205], [300, 301, 302, 303, 304, 305, 306]]
+
+        const handleRoomChange = (value) => {
+            console.log(props);
+            props.handleRoomChange(value);
+        }
+        return (
+            <>{
+                hotelrooms.map((floor) => (
+                    <div key={floor} className={styles.grid}>
+                        {
+                            floor.map((room) => (
+                                <h2 className={styles.card}
+                                    key={room}
+                                    onClick={() => handleRoomChange(room)}>
+                                    {room}
+                                </h2>
+                            ))}
+                    </div>
+                ))
+            }</>
+        );
+    }
