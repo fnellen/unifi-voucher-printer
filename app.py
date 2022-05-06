82cae351fa2e960c6e5f09f053e3f3b621fb19b3
Federico <nellenfederico@gmail.com>
2022-05-06 16:55:48 +0200

    FlaskApp for network calls

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import request
+from VoucherPrinterService import VoucherPrinterService
+from flask import abort
+import json
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/create-voucher", methods=["POST"])
+def createVoucher():
+    if request.method == "POST":
+        try:
+            minutes = request.args.get('minutes', '')
+            count = request.args.get('count', '')
+            quota = request.args.get('quota', '')
+            note = request.args.get('note', '')
+            up = request.args.get('up', None)
+            down = request.args.get('down', None)
+            megabytes = request.args.get('megabytes', None)
+            print(minutes, count, quota, note, up, down, megabytes)
+            if minutes == '' or count == '' or quota == '' or note == '':
+                abort(400)
+            voucherPrinterService = VoucherPrinterService()
+            success, vouchers = voucherPrinterService.printVouchers(
+                minutes=int(minutes), count=int(count), quota=int(quota), note=note, up=up, down=down, megabytes=megabytes)
+            if success:
+                return json.dumps(vouchers)
+            else:
+                abort(500)
+        except KeyError as err:
+            return "Error: " + str(err)
