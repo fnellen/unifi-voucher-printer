594956794b957735c88287b7ab8afa9fa98aa0f9
Federico <nellenfederico@gmail.com>
2022-05-12 18:19:20 +0200

    renamed views.py -> app.py

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 from waitress import serve
-import views
+import app
 from decouple import config
-serve(views.app, host='0.0.0.0', port=config(
+serve(app.app, host='0.0.0.0', port=config(
     "FLASK_RUN_PORT"), url_scheme="http")
